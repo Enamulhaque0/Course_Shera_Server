49c5a1d8afe3c9722885baa643dc2722c22d3e94
Enamulhaque0e <enamulhaq243@gmail.com>
2022-10-26 23:55:58 +0600

    api set

--- index.js
+++ index.js
@@ -4,3 +4,50 @@ const port = process.env.PORT || 5000;
 const cors = require("cors");
 
 app.use(cors());
+
+
+const courses = require("./data/courses.json");
+
+
+app.get("/", (req, res) => {
+    res.send("api running");
+  });
+
+
+  app.get("/courses", (req, res) => {
+    res.send(courses);
+  });
+
+
+  app.get("/course/:id", (req, res) => {
+    const course = courses.find(c=> c.id == req.params.id)
+    res.send(course)
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log("Course Sheara running ", port);
+  });
